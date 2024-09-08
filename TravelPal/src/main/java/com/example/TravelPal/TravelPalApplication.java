e852a85a9bb2d1d0cd77599a43cf6e4f5251c9e1
ryan ma <ryanma22303@gmail.com>
2024-09-08 12:08:41 -0400

    add initial repo files

---
+++ TravelPal/src/main/java/com/example/TravelPal/TravelPalApplication.java
@@ -0,0 +1,13 @@
+package com.example.TravelPal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPalApplication.class, args);
+	}
+
+}
---
+++ TravelPal/src/test/java/com/example/TravelPal/TravelPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TravelPal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
